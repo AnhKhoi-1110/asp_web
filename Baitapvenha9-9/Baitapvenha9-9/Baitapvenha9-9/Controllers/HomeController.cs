e741eec9e55d53dc2345d901e75eb9077e532e34
AnhKhoi-1110 <SD.Dragon@gmail.com>
2024-09-09 12:56:09 +0700

    BaiTapVeNha9-9

---
+++ Baitapvenha9-9/Baitapvenha9-9/Baitapvenha9-9/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Baitapvenha9_9.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Baitapvenha9_9.Controllers
+{
+
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.HoTen = "Nguyễn Hoàng Anh Khôi";
+            ViewBag.MSSV = "1822040937";
+            ViewData["Nam"] = "Năm 2004";
+            return View();
+        }
+
+
+    }
+
+}
---
+++ Baitapvenha9-9/Baitapvenha9-9/Baitapvenha9-9/Controllers/Tuan02Controller.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace baitapvenha9_9.Controllers
+{
+    public class Tuan02Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.HoTen = "Nguyễn Hoàng Anh Khôi";
+            ViewBag.MSSV = "1822040937";
+            ViewData["Nam"] = "Năm 2004";
+            return View();
+        }
+    }
+}
