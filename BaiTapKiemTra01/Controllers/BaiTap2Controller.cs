7e98ea9053d211fa8c5ab150a382d3f702809236
AnhKhoi-1110 <SD.Dragon@gmail.com>
2024-09-10 15:08:23 +0700

    BaiTapKiemTra01

---
+++ BaiTapKiemTra01/Controllers/BaiTap2Controller.cs
@@ -0,0 +1,19 @@
+﻿using BaiTapKiemTra01.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaiTapKiemTra01.Controllers
+{
+    public class BaiTap2Controller : Controller
+    {
+        public IActionResult BaiTap2()
+        {
+            var sanpham = new SanPhamViewModel
+            {
+                TenSp = " BMW ",
+
+                GiaBan = " 7 tỷ"
+            };
+            return View(sanpham);
+        }
+    }
+}
