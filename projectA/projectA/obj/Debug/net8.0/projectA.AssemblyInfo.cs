1247d9d145b6f4753409a48cf126d758f6cca34e
AnhKhoi-1110 <SD.Dragon@gmail.com>
2024-10-07 15:25:30 +0700

    BaiKiemTra03_01

--- projectA/projectA/obj/Debug/net8.0/projectA.AssemblyInfo.cs
+++ projectA/projectA/obj/Debug/net8.0/projectA.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("projectA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+947bba922ae4489b16cc6c160225895846677158")]
 [assembly: System.Reflection.AssemblyProductAttribute("projectA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("projectA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
